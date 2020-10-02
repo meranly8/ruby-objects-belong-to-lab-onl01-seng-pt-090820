f15d7b898751256e86025ef94745a0f68f216936
Michelle Ranly <meranly8@gmail.com>
2020-10-02 00:35:01 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,8 @@
 class Post 
-  attr_accessor :title
+  attr_accessor :title, :author
   
-  def initialize(author)
+  def initialize(title, author)
+    @title = title
     author = Author.new
   end
 end
